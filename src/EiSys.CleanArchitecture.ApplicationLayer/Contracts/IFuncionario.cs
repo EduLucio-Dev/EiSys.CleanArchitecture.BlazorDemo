618bd1aff6159e7653589ceb51f04bb3dd0f43d1
Eduardo Lucio <eduardo.lucio@eisys.com.br>
2024-04-01 07:34:35 -0300

    - Adicionado a inteface do Funcionario. - Aplicado o Crud

---
+++ src/EiSys.CleanArchitecture.ApplicationLayer/Contracts/IFuncionario.cs
@@ -0,0 +1,14 @@
+﻿using EiSys.CleanArchitecture.ApplicationLayer.DTOs;
+using EiSys.CleanArchitecture.DomainLayer.Entities;
+
+namespace EiSys.CleanArchitecture.ApplicationLayer.Contracts
+{
+    public interface IFuncionario
+    {
+        Task<ServiceResponse> AddAsync(Funcionario funcionario);
+        Task<ServiceResponse> UpdateAsync(Funcionario funcionario);
+        Task<ServiceResponse> DeleteAsync(Guid Id);
+        Task<List<Funcionario>> GetAsync();
+        Task<List<Funcionario>> GetByIdAsync(Guid id);
+    }
+}
