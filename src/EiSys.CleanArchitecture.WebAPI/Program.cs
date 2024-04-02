cd98ecb447ae303c1aa242ff5ee779cc3a310539
Eduardo Lucio <eduardo.lucio@eisys.com.br>
2024-04-02 07:09:32 -0300

    - Configurado a string de conexão com o banco de dados "CleanArchitectureBlazorDemoDb". - Adc ao program as configurações para a conexão com o banco de dados.

--- src/EiSys.CleanArchitecture.WebAPI/Program.cs
+++ src/EiSys.CleanArchitecture.WebAPI/Program.cs
@@ -1,3 +1,8 @@
+using EiSys.CleanArchitecture.ApplicationLayer.Contracts;
+using EiSys.CleanArchitecture.InfrastructureLayer.Data;
+using EiSys.CleanArchitecture.InfrastructureLayer.Implementations;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +12,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Pega a string de conexão pelo aplicativo appDbContext que configuramos
+builder.Services.AddDbContext<AppDbContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IFuncionario, FuncionarioRepo>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
